1675c7f857e9199e04b9b894b20d417c6218e825
Aleti Sunil <aletisunil@gmail.com>
2020-04-15 04:06:58 +0530

    Initial Commit of React ToDO APP

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import logo from './pic.png';
+import './App.css';
+
+class App extends React.Component{
+   constructor(props){
+   super(props)
+   this.state={
+     newItem:"",
+     list:[]
+   };
+  }
+  addItem(todovalue){
+    if(todovalue!==""){
+     const newItem={
+       id: Date.now(),
+       value: todovalue,
+       isdone: false
+     };
+     const list=[...this.state.list]
+     list.push(newItem);
+     this.setState({
+       list,
+       newItem:""
+     });
+    }
+  }
+  deleteItem(id){
+    const list=[...this.state.list]
+    const updatedList= list.filter(item =>item.id!==id)
+    this.setState({list: updatedList})
+  }
+  updateInput(input){
+    this.setState({
+      newItem:input
+    })
+
+  }
+
+
+
+render() {
+    return (
+      <div>
+        <img src={logo} width="120" height="120" className="logo" />
+        <h1 className="app-title">ToDo App</h1>
+        <div className="container">
+         Add an item..
+         <br/>
+        <input type="text" placeholder="write a todo" className="input-text" required value={this.state.newItem} onChange={e=> this.updateInput(e.target.value)}/>
+        <button className="add-btn"
+        onClick={()=>this.addItem(this.state.newItem)}
+        disabled={!this.state.newItem.length}>Add Todo</button>
+        <div className="list">
+          <ul>
+            {this.state.list.map(item=>{
+              return(
+                <li key={item.id}>
+                  <input type="checkbox"
+                  name="isdone"
+                  onChange={()=>{}}/>
+                  {item.value}
+                  <button className="btn"
+                  onClick={()=>this.deleteItem(item.id)}>Delete</button>
+                </li>
+              );
+            })}
+
+            
+          </ul>
+
+          </div>
+         </div>
+      </div>
+    );
+  }
+}
+
+
+export default App
